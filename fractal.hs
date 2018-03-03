ad5d9dcd38cb7755990d5ca904f685f997af5065
Laurence Emms <whatthefunctionalblog@gmail.com>
2018-03-03 01:51:29 -0800

    Incorporating feedback from u/jared--w

--- fractal.hs
+++ fractal.hs
@@ -1,6 +1,8 @@
 -- fractal.hs
 -- Create an image with a fractal
 -- Copyright Laurence Emms 2018
+--
+-- Special thanks to u/jared--w for feedback and improvements.
 
 module Main (main) where
 
@@ -9,23 +11,47 @@ import System.Exit
 import Safe
 import Codec.Picture
 
+newtype Counter = Counter Int
+newtype Height = Height Int
+newtype Width = Width Int
+
+data Dimensions = Dimensions Width Height
+data Coordinate = Coordinate Int Int
+data Point = Point Float Float
+
 printFileName :: Maybe String -> IO (Maybe String)
-printFileName Nothing = (putStrLn "Usage: fractal <output file>") >> (return Nothing)
-printFileName (Just fileName) = (putStrLn ("Generating " ++ fileName)) >> (return (Just fileName))
+printFileName Nothing = putStrLn "Usage: fractal <output file>" >> return Nothing
+printFileName (Just fileName) = putStrLn ("Generating " ++ fileName) >> return (Just fileName)
 
-saveImageFile :: Maybe String -> DynamicImage -> IO ()
-saveImageFile Nothing _ = return ()
-saveImageFile (Just fileName) image = savePngImage fileName image
+savePNGFile :: Maybe String -> DynamicImage -> IO ()
+savePNGFile Nothing _ = return ()
+savePNGFile (Just fileName) image = savePngImage fileName image
 
-genMandelbrot :: Int -> Int -> Float -> Float -> Float -> Float -> Int
-genMandelbrot iter maxIter x y px py = if (iter > maxIter) || (x * x + y * y > 4.0) then iter else (genMandelbrot (iter + 1) maxIter (x * x - y * y + px) (2.0 * x * y + py) px py)
+genMandelbrot :: Counter -> Counter -> Point -> Point -> Int
+genMandelbrot (Counter iter) (Counter maxIter) (Point x y) (Point px py)
+    | diverges || iter > maxIter = iter
+    | otherwise = genMandelbrot (Counter (iter + 1)) (Counter maxIter) (Point updatedX updatedY) (Point px py)
+    where
+        diverges = x * x + y * y > 4.0
+        --Update coordinates based on the definition of the Mandelbrot Set
+        updatedX = x * x - y * y + px
+        updatedY = 2.0 * x * y + py
 
-mandelbrot :: Int -> Int -> Int -> Int -> Int -> PixelRGB8
-mandelbrot maxIter w h x y = let p = min (genMandelbrot 0 maxIter 0.0 0.0 ((fromIntegral (x - w `div` 2)) / (fromIntegral (max w h)) * 3.0) ((fromIntegral (y - h `div` 2)) / (fromIntegral (max w h)) * 3.0)) 255
-                             in PixelRGB8 0 0 (fromIntegral p)
+mandelbrot :: Counter -> Dimensions -> Int -> Int -> PixelRGB8
+mandelbrot (Counter maxIter) (Dimensions (Width w) (Height h)) x y =
+    let maxDimension = max w h
+        --Coordinates recentered so that the origin is in the middle of the image
+        Coordinate rx ry = Coordinate (x - w `div` 2) (y - h `div` 2)
+        --Coordinates normalized into the range [-1, 1]
+        Point nx ny = Point (fromIntegral rx / fromIntegral maxDimension) (fromIntegral ry / fromIntegral maxDimension)
+        pixelValue = genMandelbrot (Counter 0) (Counter maxIter) (Point 0.0 0.0) (Point (nx * 3.0) (ny * 3.0)) --Scale point to the range [-3, 3]
+    in PixelRGB8 0 0 (fromIntegral (min pixelValue 255))
 
-main = getArgs >>=
-       (\args -> (printFileName (headMay args))) >>=
-       (\fileName -> return (fileName, (ImageRGB8 (generateImage (mandelbrot 1000 1920 1080) 1920 1080)))) >>=
-       (\(fileName, image) -> saveImageFile fileName image) >>
-       exitSuccess
+main = let imageWidth = 1920
+           imageHeight = 1080
+           fractal = mandelbrot (Counter 1000) (Dimensions (Width imageWidth) (Height imageHeight))
+       in getArgs >>=
+          (\args -> printFileName (headMay args)) >>=
+          (\fileName -> return (fileName, (ImageRGB8 (generateImage fractal 1920 1080)))) >>=
+          (\(fileName, image) -> savePNGFile fileName image) >>
+          exitSuccess
